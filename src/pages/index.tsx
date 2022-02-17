38f97dc73aa6b61aa43ad87af2310a9aadbb2cb0
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-02-16 22:11:26 -0300

    chore: handling graphql query data in next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+import { usePageQuery } from '../generated/graphql';
+
 export default function Home() {
-  return <h1>Hello world!</h1>;
+  const [{ data }] = usePageQuery({ variables: { slug: 'home' } });
+
+  return <h1>{data?.page.title}</h1>;
 }
